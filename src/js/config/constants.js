a91072fbdf6e80521bc8e502afa5a34e3a07687e
felipejeanmartinss <felipejeanmartinss@gmail.com>
2026-07-10 17:17:17 -0300

    refactoring

--- src/js/config/constants.js
+++ src/js/config/constants.js
@@ -1,1 +1,8 @@
+export const APP_CONFIG = Object.freeze({
+    name: "Disponibilidade",
 
+    description:
+        "Sistema de gestão da disponibilidade comercial de unidades.",
+
+    version: "2.0.0",
+});
